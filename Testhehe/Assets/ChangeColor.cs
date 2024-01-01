befbc3a03f12961ca0f467110c171f47ea7153da
Kou Hoang <kouhoangit@gmail.com>
2024-01-01 20:16:37 +0700

    Model test

---
+++ Testhehe/Assets/ChangeColor.cs
@@ -0,0 +1,37 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ChangeColor : MonoBehaviour
+{
+    
+    public Material[] material;
+    public int x;
+    Renderer rend;
+
+    void Start()
+    {
+        x = 0;
+        rend = GetComponent<Renderer>();
+        rend.enabled = true;
+        rend.sharedMaterial = material[x];
+
+    }
+
+    void Update()
+    {
+        rend.sharedMaterial = material[x];
+    }
+
+    public void NextColor()
+    {
+        if(x<2)
+        {
+            x++;
+        }
+        else
+        {
+            x=0;
+        }
+    }
+}
